37ff9efaa3d536d9c92802b23c7df65644259b9a
JaKu01 <91499494+JaKu01@users.noreply.github.com>
2025-06-25 18:00:22 +0200

    Implement protection mechanisms.

--- public.js
+++ public.js
@@ -4,17 +4,92 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
+const naiveBlacklist = ["http://logger"]
+const advancedBlacklist = ["http://logger", "http://172.18.0.2"]
+
 app.get('/ui', async (req, res) => {
     // Serve ui
     res.sendFile( '/app/ui/main.html');
 });
 
-app.get('/', async (req, res) => {
+app.get('/proxy', async (req, res) => {
     try {
-        const targetUrl = req.query.proxy;
+        const targetUrl = req.query.url;
 
         if (!targetUrl) {
-            return res.status(400).json({ error: 'proxy query parameter is required' });
+            return res.status(400).json({ error: 'url query parameter is required' });
+        }
+
+        const historyUrl = 'http://logger/history';
+        await axios.post(historyUrl, { entry: targetUrl });
+
+        const response = await axios.get(targetUrl);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
+app.get('/naive-blacklist', async (req, res) => {
+    try {
+        const targetUrl = req.query.url;
+
+        if (!targetUrl) {
+            return res.status(400).json({ error: 'url query parameter is required' });
+        }
+
+        if (naiveBlacklist.some(url => targetUrl.startsWith(url))) {
+            return res.status(403).json({ error: 'Hey, thats mean. You tried to access our logging, ähh nevermind.' });
+        }
+
+        const historyUrl = 'http://logger/history';
+        await axios.post(historyUrl, { entry: targetUrl });
+
+        const response = await axios.get(targetUrl);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
+app.get('/advanced-blacklist', async (req, res) => {
+    try {
+        const targetUrl = req.query.url;
+
+        if (!targetUrl) {
+            return res.status(400).json({ error: 'url query parameter is required' });
+        }
+
+        if (advancedBlacklist.some(url => targetUrl.startsWith(url))) {
+            return res.status(403).json({ error: 'Hey, thats mean. You tried to access our logging, ähh nevermind.' });
+        }
+
+        const historyUrl = 'http://logger/history';
+        await axios.post(historyUrl, { entry: targetUrl });
+
+        const response = await axios.get(targetUrl);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
+app.get('/sophisticated-blacklist', async (req, res) => {
+    try {
+        const targetUrl = req.query.url;
+
+        if (!targetUrl) {
+            return res.status(400).json({ error: 'url query parameter is required' });
+        }
+
+        if (blacklist.some(url => targetUrl.startsWith(url))) {
+            return res.status(403).json({ error: 'Hey, thats mean. You tried to access our logging, ähh nevermind.' });
         }
 
         const historyUrl = 'http://logger/history';
